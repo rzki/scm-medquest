a8051144b9e0e6f3afc7a305f932147d0a022fed
Rizky Dhani <rizkydhani15@gmail.com>
2025-03-15 17:44:04 +0700

    added Temperature Humidity model & migrations

---
+++ database/migrations/2025_03_15_103356_create_temperature_humidities_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('temperatures_humidities', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('temperatureId')->unique();
+            $table->date('date');
+            $table->date('period');
+            $table->string('location');
+            $table->string('serial_no');
+            $table->integer('observed_temperature_start');
+            $table->integer('observed_temperature_end');
+            $table->time('time_0800');
+            $table->decimal('temp_0800', 5, 2);
+            $table->integer('rh_0800');
+            $table->string('pic_0800');
+            $table->time('time_1100')->nullable();
+            $table->decimal('temp_1100', 5, 2)->nullable();
+            $table->integer('rh_1100')->nullable();
+            $table->string('pic_1100')->nullable();
+            $table->time('time_1400')->nullable();
+            $table->decimal('temp_1400', 5, 2)->nullable();
+            $table->integer('rh_1400')->nullable();
+            $table->string('pic_1400')->nullable();
+            $table->time('time_1700')->nullable();
+            $table->decimal('temp_1700', 5, 2)->nullable();
+            $table->integer('rh_1700')->nullable();
+            $table->string('pic_1700')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('temperature_humidities');
+    }
+};
