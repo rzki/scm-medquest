60e8efe89603bb285c01b391817c49137d529469
Rizky Dhani <rizkydhani15@gmail.com>
2025-04-09 13:51:32 +0700

    remove redundant table definition from TemperatureHumidity model

--- app/Models/TemperatureHumidity.php
+++ app/Models/TemperatureHumidity.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class TemperatureHumidity extends Model
 {
     protected $guarded = ['id'];
-    protected $table = 'temperatures_humidities';
 
     public function temperatureDeviations()
     {
