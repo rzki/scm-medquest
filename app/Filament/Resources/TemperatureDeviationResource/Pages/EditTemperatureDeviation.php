ae8883e404fb6a964d8c1b4b0fda9353bf883bf6
Rizky Dhani <rizkydhani15@gmail.com>
2025-04-14 15:36:04 +0700

    feat(EditTemperatureDeviation): add actions to mark records as reviewed and acknowledged by specific roles

--- app/Filament/Resources/TemperatureDeviationResource/Pages/EditTemperatureDeviation.php
+++ app/Filament/Resources/TemperatureDeviationResource/Pages/EditTemperatureDeviation.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\TemperatureDeviationResource\Pages;
 
-use App\Filament\Resources\TemperatureDeviationResource;
 use Filament\Actions;
+use Filament\Actions\Action;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\TemperatureDeviationResource;
 
 class EditTemperatureDeviation extends EditRecord
 {
@@ -13,7 +17,42 @@ class EditTemperatureDeviation extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Action::make('is_reviewed')
+                ->label('Mark as Reviewed')
+                ->visible(fn () => Auth::user()->hasRole('Supply Chain Manager'))
+                ->action(function (Model $record) {
+                    $record->update([
+                        'is_reviewed' => true,
+                        'reviewed_by' => auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y')),
+                        'reviewed_at' => now('Asia/Jakarta'),
+                    ]);
+                Notification::make()
+                    ->title('Success!')
+                    ->body('Marked as reviewed successfully by Supply Chain Manager.')
+                    ->success()
+                    ->send();
+                })
+                ->requiresConfirmation()
+                ->color('success')
+                ->icon('heroicon-o-check'),
+            Action::make('is_acknowledged')
+                ->label('Mark as Acknowledged')
+                ->visible(fn () => Auth::user()->hasRole('QA Manager'))
+                ->action(function (Model $record) {
+                    $record->update([
+                        'is_acknowledged' => true,
+                        'acknowledged_by' => auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y')),
+                        'acknowledged_at' => now('Asia/Jakarta'),
+                    ]);
+                Notification::make()
+                    ->title('Success!')
+                    ->body('Marked as acknowledged successfully by QA Manager.')
+                    ->success()
+                    ->send();
+                })
+                ->requiresConfirmation()
+                ->color('info')
+                ->icon('heroicon-o-check'),
         ];
     }
 }
