576014be30785d1a5274168457b8e85dc6a6c4f6
Rizky Dhani <rizkydhani15@gmail.com>
2025-04-24 16:41:11 +0700

    feat(CreateTemperatureDeviation): populate form with location data on mount

--- app/Filament/Resources/TemperatureDeviationResource/Pages/CreateTemperatureDeviation.php
+++ app/Filament/Resources/TemperatureDeviationResource/Pages/CreateTemperatureDeviation.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\TemperatureDeviationResource\Pages;
 
 use Carbon\Carbon;
+use App\Models\Location;
 use Illuminate\Support\Str;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\TemperatureDeviationResource;
@@ -10,10 +11,31 @@ use App\Filament\Resources\TemperatureDeviationResource;
 class CreateTemperatureDeviation extends CreateRecord
 {
     protected static string $resource = TemperatureDeviationResource::class;
+    public function mount(): void
+    {
+        parent::mount();
+
+        $locationId = request()->get('location_id');
+        $time = request()->get('time');
+        if ($locationId && $location = Location::find($locationId)) {
+            $this->form->fill([
+                'location_id' => $location->id,
+                'temperature_humidity_id' => request()->get('temp_id'),
+                'date' => Carbon::now(),
+                'time' => $time ? Carbon::createFromFormat('H:i', $time)->format('H:i') : null,
+                'serial_number' => $location->serial_number,
+                'observed_temperature' => "{$location->temperature_start}°C to {$location->temperature_end}°C",
+                'temperature_start' => $location->temperature_start,
+                'temperature_end' => $location->temperature_end,
+                'temperature_deviation' => request()->get('temperature_deviation'),
+            ]);
+        }
+    }
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['temperatureDeviationId'] = Str::orderedUuid();
         $data['date'] = Carbon::now();
+        $data['temperature_deviation'] = request()->get('temperature_deviation') ?? $data['temperature_deviation'];
         $data['pic'] = auth()->user()->initial.' ' . strtoupper(Carbon::now()->format('d M Y'));
         return $data;
     }
