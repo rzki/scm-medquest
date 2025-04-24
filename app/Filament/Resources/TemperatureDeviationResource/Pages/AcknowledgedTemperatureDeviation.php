f0e1e77fa5a8f8cd53c952b8ee63adacd9bba92f
Rizky Dhani <rizkydhani15@gmail.com>
2025-04-24 18:42:36 +0700

    feat(ReviewedTemperatureDeviation): add review actions and enhance table columns for user roles

--- app/Filament/Resources/TemperatureDeviationResource/Pages/AcknowledgedTemperatureDeviation.php
+++ app/Filament/Resources/TemperatureDeviationResource/Pages/AcknowledgedTemperatureDeviation.php
@@ -2,9 +2,22 @@
 
 namespace App\Filament\Resources\TemperatureDeviationResource\Pages;
 
-use App\Filament\Resources\TemperatureDeviationResource;
 use Filament\Actions;
+use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
+use App\Models\TemperatureDeviation;
+use Illuminate\Support\Facades\Auth;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\listRecords;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\TemperatureDeviationResource;
 
 class AcknowledgedTemperatureDeviation extends listRecords
 {
@@ -13,5 +26,85 @@ class AcknowledgedTemperatureDeviation extends listRecords
     public function getBreadcrumb(): string
     {
         return 'Pending Acknowledgement'; // or any label you want
+    }
+    
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('date')
+                    ->label('Date (Tanggal)')
+                    ->sortable()
+                    ->searchable()
+                    ->date('d/m/Y'),
+                TextColumn::make('time')
+                    ->label('Time (Jam)')
+                    ->sortable()
+                    ->searchable()
+                    ->time('H:i'),
+                TextColumn::make('temperature_deviation')
+                    ->label('Temperature Deviation (°C)'),
+                TextColumn::make('length_temperature_deviation')
+                    ->label('Length of Temperature Deviation (Menit/Jam)'),
+                TextColumn::make('deviation_reason')
+                    ->label('Reason for Deviation'),
+                TextColumn::make('pic')
+                    ->label('PIC (SCM)'),
+                TextColumn::make('risk_analysis')
+                    ->label('Risk Analysis of impact deviation'),
+                TextColumn::make('analyzer_pic')
+                    ->label('Analyzed by (QA)'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Action::make('is_acknowledged')
+                    ->label('Mark as Acknowledged')
+                    ->visible(function (TemperatureDeviation $record) {
+                        $isAcknowledged = $record->is_acknowledged == false;
+                        $admin = Auth::user()->hasRole(['Super Admin', 'QA Manager']);
+                        return $isAcknowledged && $admin;
+                    })
+                    ->action(function (Model $record) {
+                        $record->update([
+                            'is_acknowledged' => true,
+                            'acknowledged_by' => auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y')),
+                            'acknowledged_at' => now('Asia/Jakarta'),
+                        ]);
+                    Notification::make()
+                        ->title('Success!')
+                        ->body('Marked as acknowledged successfully by QA Manager.')
+                        ->success()
+                        ->send();
+                    })
+                    ->requiresConfirmation()
+                    ->color('info')
+                    ->icon('heroicon-o-check'),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                BulkAction::make('is_acknowledged')
+                    ->label('Mark as Acknowledged')
+                    ->icon('heroicon-o-check-badge')
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn() => Auth::user()->hasRole(['QA Manager']))
+                    ->action(function (Collection $records) {
+                        foreach ($records as $record) {
+                            $record->is_acknowledged = true;
+                            $record->acknowledged_by = auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y'));
+                            $record->acknowledged_at = now('Asia/Jakarta');
+                            $record->save();
+                        }
+                        
+                        Notification::make()
+                            ->title('Success!')
+                            ->body('Selected data marked as acknowledged successfully')
+                            ->success()
+                            ->send();
+                    }),
+                ]),
+            ]);
     }
 }
--- app/Filament/Resources/TemperatureDeviationResource/Pages/ReviewedTemperatureDeviation.php
+++ app/Filament/Resources/TemperatureDeviationResource/Pages/ReviewedTemperatureDeviation.php
@@ -2,9 +2,22 @@
 
 namespace App\Filament\Resources\TemperatureDeviationResource\Pages;
 
-use App\Filament\Resources\TemperatureDeviationResource;
 use Filament\Actions;
+use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
+use App\Models\TemperatureDeviation;
+use Illuminate\Support\Facades\Auth;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\TemperatureDeviationResource;
 
 class ReviewedTemperatureDeviation extends ListRecords
 {
@@ -13,5 +26,85 @@ class ReviewedTemperatureDeviation extends ListRecords
     public function getBreadcrumb(): string
     {
         return 'Pending Review'; // or any label you want
+    }
+    
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('date')
+                    ->label('Date (Tanggal)')
+                    ->sortable()
+                    ->searchable()
+                    ->date('d/m/Y'),
+                TextColumn::make('time')
+                    ->label('Time (Jam)')
+                    ->sortable()
+                    ->searchable()
+                    ->time('H:i'),
+                TextColumn::make('temperature_deviation')
+                    ->label('Temperature Deviation (°C)'),
+                TextColumn::make('length_temperature_deviation')
+                    ->label('Length of Temperature Deviation (Menit/Jam)'),
+                TextColumn::make('deviation_reason')
+                    ->label('Reason for Deviation'),
+                TextColumn::make('pic')
+                    ->label('PIC (SCM)'),
+                TextColumn::make('risk_analysis')
+                    ->label('Risk Analysis of impact deviation'),
+                TextColumn::make('analyzer_pic')
+                    ->label('Analyzed by (QA)'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Action::make('is_reviewed')
+                    ->label('Mark as Reviewed')
+                    ->visible(function (TemperatureDeviation $record) {
+                        $isReviewed = $record->is_reviewed == false;
+                        $admin = Auth::user()->hasRole(['Super Admin', 'Supply Chain Manager']);
+                        return $isReviewed && $admin;
+                    })
+                    ->action(function (Model $record) {
+                        $record->update([
+                            'is_reviewed' => true,
+                            'reviewed_by' => auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y')),
+                            'reviewed_at' => now('Asia/Jakarta'),
+                        ]);
+                    Notification::make()
+                        ->title('Success!')
+                        ->body('Marked as reviewed successfully by Supply Chain Manager.')
+                        ->success()
+                        ->send();
+                    })
+                    ->requiresConfirmation()
+                    ->color('success')
+                    ->icon('heroicon-o-check'),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    BulkAction::make('is_reviewed')
+                    ->label('Mark as Reviewed')
+                    ->icon('heroicon-o-check-badge')
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn() => Auth::user()->hasRole(['Supply Chain Manager']))
+                    ->action(function (Collection $records) {
+                        foreach ($records as $record) {
+                            $record->is_reviewed = true;
+                            $record->reviewed_by = auth()->user()->initial . ' ' . strtoupper(now('Asia/Jakarta')->format('d M Y'));
+                            $record->reviewed_at = now('Asia/Jakarta');
+                            $record->save();
+                        }
+                        
+                    Notification::make()
+                        ->title('Success!')
+                        ->body('Selected data marked as reviewed successfully')
+                        ->success()
+                        ->send();
+                    }),
+                ]),
+            ]);
     }
 }
