63c8b002a3e6eaa3301fcb88b852227e58c6b7d3
Rizky Dhani <rizkydhani15@gmail.com>
2025-04-25 16:45:49 +0700

    feat(ListTemperatureHumidities): enhance header actions with labeled create button for temperature & humidity

--- app/Filament/Resources/TemperatureHumidityResource/Pages/ListTemperatureHumidities.php
+++ app/Filament/Resources/TemperatureHumidityResource/Pages/ListTemperatureHumidities.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Resources\TemperatureHumidityResource\Pages;
 
-use App\Filament\Resources\TemperatureHumidityResource;
 use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\TemperatureHumidityResource;
 
 class ListTemperatureHumidities extends ListRecords
 {
@@ -17,7 +18,9 @@ class ListTemperatureHumidities extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make()
+            ->label('New Temperature & Humidity')
+            ->color('success'),
         ];
     }
 }
