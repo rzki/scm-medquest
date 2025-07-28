9b385f4904eb5860c68bed02c0a2544b3f089269
Rizky Dhani <rizkydhani15@gmail.com>
2025-07-28 08:40:29 +0700

    feat: Add Livewire update route configuration in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Services\TemperatureHumidityNotificationService;
+use Livewire\Livewire;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Livewire::setUpdateRoute(function ($handle) {
+            return Route::post('/scm-medquest/livewire/update', $handle);
+        });
         Gate::before(function (User $user, string $ability) {
             return $user->isSuperAdmin() ? true: null;
         });
